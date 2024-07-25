ed49b5b72b2decb7f548c565061a389cc98040a3
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-07-25 21:52:49 +0600

    pattern::cont10

---
+++ pattern2.cpp
@@ -0,0 +1,171 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    //*
+    //**
+    //*** 
+    //****
+    // int n;
+    // cin>>n;
+    // int row=1;
+    // while(row<=n)
+    // {
+    //     int col = 1;
+    //     while(col<=row)
+    //     {
+    //         cout<<'*';
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+    //1
+    //12
+    //123 
+    //1234
+    // int n;
+    // cin>>n;
+    // int row=1;
+    // while(row<=n)
+    // {
+    //     int col = 1;
+    //     while(col<=row)
+    //     {
+    //         cout<<col;
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+
+    // 1
+    // 23
+    // 456
+    // 78910
+    // int n;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // int row=1;
+    // int count=1;
+    // while(row<=n)
+    // {
+    //     int col = 1;
+    //     while(col<=row)
+
+    //     {
+    //         cout<<count<<" ";
+    //         count++;
+    //         col++;
+
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+//     //1
+//     //23
+//     //345
+//     //4567
+//     int n;
+//     cin>>n;
+//     int row=1;
+//     while(row<=n)    
+//     { 
+//       int col=1;
+//     //   int val=row;
+//       while(col<=row)
+//       {
+//         cout<<row+col-1;
+//         // val++;
+//         col++;
+
+//       }
+
+//       cout<<endl;
+//       row++;
+//     }
+
+
+
+//1 
+//21 
+//321 
+//4321
+
+
+
+//    int n;
+//    cin>>n;
+//    int row=1;
+
+//     while(row<=n)
+//     {
+//         int col=1;
+//         while(col<=row)
+//         {
+//             cout<<row-col+1<<' ';
+//             col++;
+
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+
+
+//AAA
+//BBB
+//CCC
+// int n;
+// cin>>n;
+// int row=1;
+
+// while(row<=n)
+//     {
+//         int col=1;
+//         while(col<=n)
+//         {   
+//             char ch='A'+row-1;
+//             cout<<ch<<" ";
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+//ABC
+//ABC
+//ABC
+int n;
+cin>>n;
+int row=1;
+char start='A';
+
+while(row<=n)
+    {
+        int col=1;
+        while(col<=n)
+        {   
+            
+            cout<<start<<" ";
+            start+=1;
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
