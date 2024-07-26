a782baf14630ad20eaaf119731e7d9449abb84dd
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-07-26 23:03:16 +0600

    added:pattern prac last

--- pattern2.cpp
+++ pattern2.cpp
@@ -221,24 +221,217 @@ int main()
 //A
 //BC
 //DEF
-    int n;
-    cin>>n;
-    int row=1;
-    char val='A';
+    // int n;
+    // cin>>n;
+    // int row=1;
+    // char val='A';
 
-    while(row<=n)
-    {
-        int col=1;
-        while(col<=row)
-        {    
+    // while(row<=n)
+    // {
+    //     int col=1;
+    //     while(col<=row)
+    //     {    
             
-            cout<<val;
-            val++;
-            col++;
-        }
-        cout<<endl;
-        row++;
-    }
+    //         cout<<val;
+    //         val++;
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+// //A
+// //BC
+// //CDE
+// //DEFG
+//     int n;
+//     cin>>n;
+//     int row=1;
+    
+
+//     while(row<=n)
+//     {
+//         int col=1;
+//         while(col<=row)
+//         {    
+            
+//             cout<<char('A'+row+col-2);
+           
+//             col++;
+//         }
+//         cout<<endl;
+//         row++;
+//     }
+
+//D
+//CD
+//BCD
+//ABCD
+    // int n;
+    // cin>>n;
+    // int row=1;
+    
+
+    // while(row<=n)
+    // {
+    //     int col=1;
+    //     char start='A'+n-row;
+    //     while(col<=row)
+    //     {    
+            
+    //         cout<<start;
+    //        start=start+1;
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+//ABC
+//BCD
+//CDE
+
+    // int n;
+    // cin>>n;
+    // int row=1;
+    
+
+    // while(row<=n)
+    // {
+    //     int col=1;
+    //     char start='A'+row-1;
+    //     while(col<=n)
+    //     {    
+            
+    //         cout<<start;
+    //        start=start+1;
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+//   int n;
+//   cin>>n;
+//   int row=1;
+//   while(row<=n)
+//   { int space=n-row;
+//     while(space)
+//     {
+//         cout<<" ";
+//         space--;
+//     }
+
+//     int col=1;
+//     while(col<=row)
+//     {
+//         cout<<'*';
+//         col++;
+//     }
+//     cout<<endl;
+//     row++;
+//   }
+
+//**** 
+//***
+//**
+//*
+
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n)
+// {
+
+// int col=1;
+// while(col<=n-row+1)
+// {
+//     cout<<"*";
+//     col++;
+
+// }
+// cout<<endl;
+// row++;
+
+// }
+
+//**** 
+// ***
+//  **
+//   *
+
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n)
+// {
+//     int space=row-1;
+//     while(space)//runs till space is non zero
+//     {
+//         cout<<' ';
+//         space--;
+
+//     }
+//     int col=1;
+    
+//     while(col<=n-row+1)
+//     {
+//         cout<<row+col-1;
+       
+//         col++;
+//     }
+//     cout<<endl;
+//     row++;
+
+// }
+
+
+//    1
+//   121
+//  12321
+// 1234321
+int n;
+cin>>n;
+int row=1;
+while(row<=n)
+
+{ //spaces
+
+int space=n-row;
+while(space)
+{
+    cout<<' ';
+    space--;
+}
+
+//1st triangle
+
+int col=1;
+while(col<=row)
+{
+    cout<<col;
+    col++;
+}
+
+//2nd triangle
+int start=row-1;
+while(start)
+{ 
+    cout<<start;
+    start--;
+
+}
+
+cout<<endl;
+row++;
+
+
+}
+
 
 
 }
