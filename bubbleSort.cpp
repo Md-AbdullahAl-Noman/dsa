7312aef90f14f417549fd95eadc642e5a7617080
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-08-24 13:32:22 +0600

    space and time complexity added of bubble sort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,13 +9,21 @@ int main()
 {
     int array[] = {50, 4, 13, 20, 11};
     int size=sizeof(array)/sizeof(array[0]);
-
+    bool swappedFlag=false;
     for(int i=1;i<size;i++)
-    {
+    {   swappedFlag=false;
         for(int j=0;j<size-i;j++)
         {
             if(array[j]>array[j+1])
-                swap(array[j],array[j+1]);
+                {   
+
+                    swap(array[j],array[j+1]);
+                    swappedFlag=true;
+                }
+        }
+        if(!swappedFlag)
+         {
+            break;
         }
     }
 
@@ -27,6 +35,7 @@ int main()
     }
 
 
-
-    return 0;
+//as there is no new round in bubble sort so space  complexity will be O(1)
+//worst time complexity will be O(n^2)as in every round one element will be compared and swapped-->n(n-1)/2 -->O(n^2)
+//best time complexity will be O(n) means the array is already sorted hence the number of round will be 1 and comparison will be n-1-->O(n)
 }
