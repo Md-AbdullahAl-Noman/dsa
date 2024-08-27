78997298f217839a363633268e9c2dc5f4150d80
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-08-27 22:27:20 +0600

    set added to STL

---
+++ set.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<set>
+using namespace std;
+
+
+int main(){
+
+ set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(5);
+    s.insert(15);
+    s.insert(15);
+    s.insert(25);
+    s.insert(35);
+
+    for(auto v:s)
+    {
+        cout<<v<<" ";
+    }
+    cout<<endl;
+    cout<<s.size()<<endl;
+    set<int>::iterator it=s.begin();
+    cout<<*it<<endl;
+    // cout<<s.find(15)<<endl;
+    s.erase(s.find(5),s.end());
+    for(auto v:s)
+    {
+        cout<<v<<" ";
+    }
+    cout<<endl;
+
+}
