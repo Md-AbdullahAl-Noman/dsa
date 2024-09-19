da4c05c6c1e72fc9affee3ab8e67b5a824ac65b8
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-19 15:31:08 +0600

    null character understood

---
+++ string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    char charname[20] = {'N', 'o', '\0','m', 'a', 'n', '\0'};
+    cout<<charname;
+    cout<<endl;
+    string name={'N','O','\0','M','A','N'};
+    cout<<name;
+
+    return 0;
+}
