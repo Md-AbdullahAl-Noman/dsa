7418ca6cbaa8ddca9668eb8af9f6b885ad64d49b
Md Abdullah Al Noman <abdullahnoman1414@gmail.com>
2024-09-20 20:08:01 +0600

    one approach to reverse string words

--- string.cpp
+++ string.cpp
@@ -1,14 +1,34 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    
-    char charname[20] = {'N', 'o', '\0','m', 'a', 'n', '\0'};
-    cout<<charname;
-    cout<<endl;
-    string name={'N','O','\0','M','A','N'};
-    cout<<name;
+string reverseWords(string& s) {
+    vector<string> words;
+    stringstream ss(s);
+    string word;
 
+    // Split the sentence into words
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    // Reverse the words vector
+    reverse(words.begin(), words.end());
+
+    // Join the reversed words into a sentence
+    string reversedSentence;
+    for (size_t i = 0; i < words.size(); ++i) {
+        reversedSentence += words[i];
+        if (i != words.size() - 1) {
+            reversedSentence += " ";
+        }
+    }
+
+    return reversedSentence;
+}
+
+int main() {
+    string s = "The Sky is blue";
+    cout << reverseWords(s);
     return 0;
 }
